81dc023983a185bf46e84ea34c9eff4bc1966e3a
gueretamartin <guereta.martin@gmail.com>
2016-04-13 01:02:47 -0300

    Modified index, add accordion, delete innecesary js files

--- WebContent/js/desplegadores.js
+++
@@ -1,6 +0,0 @@
- $(document).ready(function(){
-    $("#desplegadoca").hide();
-    $("#desplegadorca").click(function(){
-        $("#desplegadoca").slideToggle("slow");
-         });
-    });
--- WebContent/js/valida.js
+++
@@ -1,6 +0,0 @@
-function validacion(f)  {
-	valor = document.getElementById("campo_numerico").value;
-if (isNaN(valor)) {
-alert("Error:\nEste campo debe tener sólo números.");
-f.valor.focus();
-return (false);}}
